328b1e92c5991df02c55ea742f122810b66fc573
Shioji Kenta <skenta0518@yahoo.co.jp>
2021-05-23 18:15:47 +0900

    S3が本番環境修正　#24

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if Rails.env.production? || Rails.env.development?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'farmland55'
@@ -11,8 +11,8 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIARH5UR2E4C43RZ373',
-      aws_secret_access_key: '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',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
   else

c7920650814ddb00e983566e5e42432eab1547e0
Shioji Kenta <skenta0518@yahoo.co.jp>
2021-05-28 13:28:15 +0900

    ゲストログイン機能追加 #34

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def new_guest
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.username = "ゲストユーザー"
+      user.profile = '私はゲストユーザーです'
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
